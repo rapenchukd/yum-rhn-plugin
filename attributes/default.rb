32819cbd742e6228e892bad10e0f3b8492b4a932
rapenchukd <rapenchuk@linux.com>
2015-02-24 13:46:59 -0500

    Made defaults quoted string for safety.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default['yum-rhn']['source'] = nil
-default['yum-rhn']['main']['enabled'] = 1
-default['yum-rhn']['main']['gpg-check'] = 1
+default['yum-rhn']['main']['enabled'] = '1'
+default['yum-rhn']['main']['gpg-check'] = '1'
 default['yum-rhn']['data_bag'] = nil
