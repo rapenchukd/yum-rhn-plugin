4bfd8209bb327560eac03c5ee818b541f257d81b
Drew Rapenchuk <drapenchuk@bloomberg.net>
2015-04-20 09:28:59 -0400

    Made idempotent

--- recipes/config.rb
+++ recipes/config.rb
@@ -17,39 +17,42 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-cache_path = Chef::Config['file_cache_path']
-
-template "#{cache_path}/0main.rhn.conf" do
-  source 'rhnplugin.conf.erb'
-  variables(
-    channel: 'main',
-    enabled: node['yum-rhn']['main']['enabled'],
-    gpg_check: node['yum-rhn']['main']['gpg-check']
-  )
-  action :create
-end
-
-unless node['yum-rhn']['data_bag'].nil?
-  data_bag(node['yum-rhn']['data_bag']).each do |value|
-    item = data_bag_item(node['yum-rhn']['data_bag'], value)
-    template "#{cache_path}/value.rhn.conf" do
-      source 'rhnplugin.conf.erb'
-      variables(
-        channel: item['channel'],
-        is_enabled: item['enabled'],
-        gpg_check: item['gpg-check']
-      )
-    end
-  end
-end
-
 file "/etc/yum/pluginconf.d/rhnplugin.conf" do
   owner 'root'
   group 'root'
   mode '0644'
-  action :touch
+  action :create
 end
 
-execute "cat #{Chef::Config['file_cache_path']}/*.rhn.conf > /etc/yum/pluginconf.d/rhnplugin.conf" do
+cache_path = Chef::Config['file_cache_path']
+
+included_files = []
+
+unless node['yum-rhn']['data_bag'].nil?
+  data_bag(node['yum-rhn']['data_bag']).each do |value|
+    item = data_bag_item(node['yum-rhn']['data_bag'], value)
+    filename = template "#{cache_path}/value.rhn.conf" do
+      source 'rhnplugin.conf.erb'
+      variables(
+        channel: item['channel'],
+        is_enabled: item['enabled'],
+        gpg_check: item['gpg-check'],
+        included_files: nil
+      )
+    end
+    included_files << filename.path
+  end
 end
+
+template "/etc/yum/pluginconf.d/rhnplugin.conf" do
+  source 'rhnplugin.conf.erb'
+  variables(
+    channel: 'main',
+    enabled: node['yum-rhn']['main']['enabled'],
+    gpg_check: node['yum-rhn']['main']['gpg-check'],
+    included_files: included_files
+  )
+  action :create
+end
+
 
